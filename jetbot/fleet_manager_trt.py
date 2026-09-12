7148105d838fb4dea6b90d5c20d327c7711a16ee
Cuter <cuter9@msn.com>
2026-09-12 12:46:35 +0800

    revise

--- jetbot/fleet_manager_trt.py
+++ jetbot/fleet_manager_trt.py
@@ -165,7 +165,7 @@ class FleeterTRT(ObjectFollower, RoadCruiserTRT):
         # otherwise, steer towards target
         # move the robot forward and steer proportional target's x-distance from center
         center = object_center_detection(self.closest_object)
-        # print(f"center: {center}; target box (x_r, y_t, x_l, y_b):{self.closest_object['bbox']}")
+        print(f"center: {center}; target box (x_r, y_t, x_l, y_b):{self.closest_object['bbox']}")
         # the speed limit is set by alpha value of Motor Class in robot.py to 0.8
         left_motor = max(min(float(self.speed_fm + self.turn_gain_fm * center[0] + self.steering_bias_fm), 1.0), -1.0)
         right_motor = max(min(float(self.speed_fm - self.turn_gain_fm * center[0] + self.steering_bias_fm), 1.0), -1.0)
